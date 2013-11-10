fb3f5221834893758b7097de9c5d4f2d71d67b3e
Zach Brown <Zbob750@live.com>
2013-11-10 00:02:52 -0600

    Back key no longer goes back a page Now that we have the awesome-sauce that is the actionbar, just use the actionbar back button to do that. Hitting the back key now will take you back to the session list.

--- src/com/zbrown/droidsteal/activities/HijackActivity.java
+++ src/com/zbrown/droidsteal/activities/HijackActivity.java
@@ -108,15 +108,6 @@ public class HijackActivity extends Activity implements Constants {
         });
     }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
-            webview.goBack();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-
     //Menu Items
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
